3312d4a369aa117f1d54b4fddbd3ff854283f914
DESKTOP-OMC2PED\Mikhail Li (Jiub) <li_m@bizplate.ru>
2018-05-29 11:33:04 +0300

    Отображение прогресса счетчика в списке

--- app/src/main/java/com/mashushka/mashushka/ui/holders/CounterHolder.java
+++ app/src/main/java/com/mashushka/mashushka/ui/holders/CounterHolder.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import com.mashushka.mashushka.R;
 import com.mashushka.mashushka.data.Counter;
 import com.mashushka.mashushka.data.entity.CounterEntity;
+import com.mashushka.mashushka.ui.CircleProgressBar;
 import com.mashushka.mashushka.ui.holders.base.BaseViewHolder;
 import com.mashushka.mashushka.ui.listeners.CounterOpener;
 
@@ -29,6 +30,7 @@ public class CounterHolder extends BaseViewHolder<CounterEntity>{
     @BindView(R.id.counter) TextView counter;
     @BindView(R.id.create_date) TextView createDate;
     @BindView(R.id.tv_days_title) TextView daysTitle;
+    @BindView(R.id.custom_progressBar) CircleProgressBar progress;
 
     public CounterHolder (View itemView, CounterOpener listener) {
         super(itemView);
@@ -56,7 +58,7 @@ public class CounterHolder extends BaseViewHolder<CounterEntity>{
         long days;
 
         days = time / (3600 * 24);
-//        hours = (time - (days * (3600 * 24))) / (3600);
+        hours = (time - (days * (3600 * 24))) / (3600);
 //        minutes = (time - (days * (3600 * 24)) - (hours * 3600)) / (60);
 //        seconds =  (time - days * (3600 * 24) -  hours * 3600 - minutes  * 60);
 
@@ -64,7 +66,7 @@ public class CounterHolder extends BaseViewHolder<CounterEntity>{
         counter.setText(String.valueOf(days)/* + " " + hours + " " + minutes + " " + seconds*/);
 
         daysTitle.setText(daysTitle.getContext().getResources().getQuantityText(R.plurals.days, (int) days));
-
+        progress.setProgress(hours);
         wrapper.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
